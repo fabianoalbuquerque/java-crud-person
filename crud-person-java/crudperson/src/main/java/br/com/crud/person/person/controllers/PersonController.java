998d86b28524e88c740c8a7382c116ed67221738
fabianoalbuquerque <fabiano.santos@sensedia.com>
2022-04-14 14:56:00 -0300

    feat: implements methods http and controllers and services

---
+++ crud-person-java/crudperson/src/main/java/br/com/crud/person/person/controllers/PersonController.java
@@ -0,0 +1,33 @@
+package br.com.crud.person.person.controllers;
+
+import br.com.crud.person.person.model.Person;
+import br.com.crud.person.person.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonService service;
+
+    @RequestMapping(value = "/{id}", method=RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable("id") String id) {
+        return service.findById(id);
+    }
+
+    @RequestMapping(method=RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
+    @RequestMapping(method=RequestMethod.POST, produces= MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+}
