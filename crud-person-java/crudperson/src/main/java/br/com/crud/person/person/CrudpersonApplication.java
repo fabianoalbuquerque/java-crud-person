2776c32e7a83312e90b9adfbee1b46a9a460d176
fabianoalbuquerque <fabiano.santos@sensedia.com>
2022-04-14 11:43:37 -0300

    general: initial project commit

---
+++ crud-person-java/crudperson/src/main/java/br/com/crud/person/person/CrudpersonApplication.java
@@ -0,0 +1,13 @@
+package br.com.crud.person.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudpersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudpersonApplication.class, args);
+	}
+
+}
---
+++ crud-person-java/crudperson/src/test/java/br/com/crud/person/person/CrudpersonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crud.person.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudpersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
